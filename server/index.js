aa41ef7ed63812d5b92b0bfd326a0cc7aa9ba59f
Latief Irfansyah <latifirfansyah@gmail.com>
2022-01-19 11:25:42 +0700

    add model, create, and get

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import todosRoutes from "./routes/todos.js";
 const app = express();
 dotenv.config();
 app.use(express.json({extended:true}))
 app.use(express.urlencoded({extended:true}))
 app.use(cors());
+app.use('/todos', todosRoutes)
 const mongodb = "mongodb+srv://ckmobile:ckmobile123@cluster0.ljkoi.mongodb.net/todos-database?retryWrites=true&w=majority"
 
 app.get('/', (req,res)=>{
@@ -16,5 +18,3 @@ app.get('/', (req,res)=>{
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 mongoose.connect(mongodb).then(()=>console.log(`server is running on port ${PORT}`)).catch(err => console.log(err))
-
-test
